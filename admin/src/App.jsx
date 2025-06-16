48d3f8effd599f6060152257425cea99e6827343
mohdamaan069 <118156938+mohdamaan069@users.noreply.github.com>
2025-06-16 12:25:31 +0530

    deleted unnecessary imports

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import { useContext } from 'react'
 import { DoctorContext } from './context/DoctorContext';
 import { AdminContext } from './context/AdminContext';
 import { Route, Routes } from 'react-router-dom'
