23d84f3ac4385520ab28c9016cf933f2e83ecc4a
mohdamaan069 <118156938+mohdamaan069@users.noreply.github.com>
2025-06-16 13:06:50 +0530

    fixed top scroll

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,12 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import AppContextProvider from './context/AppContext.jsx'
+import ScrollToTop from './components/ScrollToTop.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <AppContextProvider>
+      <ScrollToTop />
       <App />
     </AppContextProvider>
   </BrowserRouter>,
